2160a1f2263fcbfad79031f129be87b378fc85cd
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 12:29:46 -0500

    add loadDataConfig, setRank, and getRank

--- TAKRanks/src/main/java/net/theakingdom/TAKRanksMain.java
+++ TAKRanks/src/main/java/net/theakingdom/TAKRanksMain.java
@@ -2,6 +2,9 @@ package net.theakingdom;
 
 import java.io.File;
 import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,6 +19,7 @@ public class TAKRanksMain extends JavaPlugin {
             this.getDataFolder().mkdir();
         }
 
+        this.loadDataConfig();
         if (dataConfig.getConfigurationSection("ranks") == null) {
             dataConfig.set("ranks.Owner.prefix", "§x§F§E§9§1§9§F§[§x§5§4§F§9§B§AOwner§x§F§E§9§1§9§F]");
             dataConfig.set("ranks.Admin.prefix", "§x§5§4§F§9§B§A[§x§F§E§9§1§9§FAdmin§x§5§4§F§9§B§A]");
@@ -26,5 +30,31 @@ public class TAKRanksMain extends JavaPlugin {
                 e.printStackTrace();
             }
         }
+    }
+
+    public FileConfiguration loadDataConfig() {
+        data = new File(this.getDataFolder(), "ranks.yml");
+        if (!data.exists()) {
+            try {
+                data.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        dataConfig = YamlConfiguration.loadConfiguration(data);
+        return dataConfig;
+    }
+
+    public static void setRank(String player, String rank) {
+        dataConfig.set("player." + Bukkit.getPlayer(player).getUniqueId().toString(), rank);
+        try {
+            dataConfig.save(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getRank(String player) {
+        return dataConfig.getString("player." + Bukkit.getPlayer(player).getUniqueId().toString());
     }
 }
