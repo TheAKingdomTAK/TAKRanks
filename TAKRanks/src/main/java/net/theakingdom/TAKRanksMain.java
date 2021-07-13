4c1cfd6e387ef2bfd84dcf52ebf04cb9e8c7f462
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 12:10:03 -0500

    starting plugin

--- TAKRanks/src/main/java/net/theakingdom/TAKRanksMain.java
+++ TAKRanks/src/main/java/net/theakingdom/TAKRanksMain.java
@@ -1,25 +1,29 @@
 package net.theakingdom;
 
+import java.io.File;
+import java.io.IOException;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TAKRanksMain implements JavaPlugin
-{
+
+public class TAKRanksMain extends JavaPlugin {
     public static File data;
     public static YamlConfiguration dataConfig;
 
     public void onEnable() {
         System.out.println("Ranks Enabling...");
         if (!this.getDataFolder().exists()) {
-            this.getDataFolder.mkdir();
+            this.getDataFolder().mkdir();
         }
 
-        this.loadDataConfig();
         if (dataConfig.getConfigurationSection("ranks") == null) {
             dataConfig.set("ranks.Owner.prefix", "§x§F§E§9§1§9§F§[§x§5§4§F§9§B§AOwner§x§F§E§9§1§9§F]");
             dataConfig.set("ranks.Admin.prefix", "§x§5§4§F§9§B§A[§x§F§E§9§1§9§FAdmin§x§5§4§F§9§B§A]");
 
             try {
-                dataConfig.save(dataFile);
+                dataConfig.save(data);
             } catch (IOException e) {
                 e.printStackTrace();
             }
